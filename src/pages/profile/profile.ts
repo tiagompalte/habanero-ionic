594a28ab120b57c1cacaef016785df81330bf92d
Tiago M Palte <tiagompalte@hotmail.com>
2019-03-10 09:17:39 -0300

    Implements profile page

---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { StorageService } from '../../services/storage.service';
+import { ClientDTO } from '../../models/client.dto';
+import { ClientService } from '../../services/domain/client.service';
+import { API_CONFIG } from '../../config/api.config';
+
+@IonicPage()
+@Component({
+  selector: 'page-profile',
+  templateUrl: 'profile.html',
+})
+export class ProfilePage {
+
+  client: ClientDTO;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public storage: StorageService, public clientService : ClientService) {
+  }
+
+  ionViewDidLoad() {
+    let localUser = this.storage.getLocalUser();
+    if(localUser && localUser.email) {
+      this.clientService.findByEmail(localUser.email).subscribe(response => {
+        this.client = response;
+        this.getImageIfExists();
+      }, error => {});
+    }
+  }
+
+  getImageIfExists() {
+    this.clientService.getImageFromBucket(this.client.id)
+    .subscribe(response => {
+      this.client.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.client.id}.jpg`;
+    }, error => {});
+  }
+
+}
